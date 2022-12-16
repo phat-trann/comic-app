f0c36caeb10c66761e892f0a57843b57fa6982fa
phat.tran <phat.tran.fit@gmail.com>
2022-12-16 18:01:34 +0700

    - Custom Carousel - doing

--- src/elements/AutoCarousel/index.tsx
+++ src/elements/AutoCarousel/index.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import { useWindowSize } from '~/hooks';
+import ChevronLeft from '~/icons/ChevronLeft';
+import ChevronRight from '~/icons/ChevronRight';
 
 const Carousel = <T,>({
   id,
@@ -15,25 +17,26 @@ const Carousel = <T,>({
   const carousel = useRef<HTMLDivElement | null>(null);
   const [windowSize, currentBreakPoint] = useWindowSize();
 
-  useEffect(() => {
-    // const autoTimeout = setTimeout(() => {
-    //   console.log(autoTimeout);
-    // }, 1000);
-    const handleScroll = (e: any) => {
-      console.log(e);
-    };
+  const handleScroll = (e) => {
+    console.log(e);
+  };
 
-    console.log(carousel.current)
+  // useEffect(() => {
+  //   // const autoTimeout = setTimeout(() => {
+  //   //   console.log(autoTimeout);
+  //   // }, 1000);
 
-    if (carousel.current) {
-      carousel.current.addEventListener('scroll:carousel', handleScroll, true);
-    }
+  //   console.log(carousel.current);
 
-    return () => {
-      // clearTimeout(autoTimeout);
-      carousel.current?.removeEventListener('scroll:carousel', handleScroll);
-    };
-  });
+  //   if (carousel.current) {
+  //     carousel.current.addEventListener('scroll:carousel', handleScroll, true);
+  //   }
+
+  //   return () => {
+  //     // clearTimeout(autoTimeout);
+  //     carousel.current?.removeEventListener('scroll:carousel', handleScroll);
+  //   };
+  // });
 
   const handleGoToSection = (event: React.MouseEvent<HTMLAnchorElement>, link?: string) => {
     event.preventDefault();
@@ -42,47 +45,27 @@ const Carousel = <T,>({
   };
   return (
     <div className="relative flex w-full flex-wrap">
-      <div className="carousel-center carousel w-screen" ref={carousel}>
+      <div className="carousel-center carousel w-screen" onScroll={handleScroll}>
         {dataList.map((comic: T, index: number) =>
           render([comic, index, [id, index + 1].join('-'), 'carousel-item ']),
         )}
       </div>
       <div className="pointer-events-none absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
         <a
-          href={leftLink}
           className={`btn-circle btn pointer-events-auto h-8 min-h-0 w-8${
             !leftLink || currentBreakPoint < 2 ? ' invisible' : ''
           }`}
           onClick={(e) => handleGoToSection(e, leftLink)}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-5 w-5"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
+          <ChevronLeft />
         </a>
         <a
-          href={rightLink}
           className={`btn-circle btn pointer-events-auto h-8 min-h-0 w-8${
             !rightLink || currentBreakPoint < 2 ? ' invisible' : ''
           }`}
           onClick={(e) => handleGoToSection(e, rightLink)}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-5 w-5"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
+          <ChevronRight />
         </a>
       </div>
     </div>
