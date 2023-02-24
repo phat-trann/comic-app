a9d4c9469533bc1b835a1058ab6aad508da585a9
phat.tran <phat.tran.fit@gmail.com>
2023-02-24 12:52:46 +0700

    - Update logics

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Routes from '~/routers';
 
 function App() {
   return (
-    <div className="App bg-stone-100">
+    <div className="App bg-stone-100 dark:bg-slate-900">
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,24 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
-import { AppContext } from '~/context/AppContext';
 
 const Footer = () => {
   const logoName = '/images/logo.png';
   const { t } = useTranslation();
-  const { resetHomepage, currentPage } = useContext(AppContext);
-  const navigate = useNavigate();
-  const onClickSection = (location: string) => {
-    if (location === '/') resetHomepage(currentPage);
-    navigate(location);
-  };
 
   return (
     <div className="border-t-8 border-orange-600 bg-white">
       <div className="flex flex-wrap p-4">
         <div className="flex w-full py-2">
-          <div className="flex w-1/3 items-center dark:invert" onClick={() => onClickSection('/')}>
-            <img src={logoName} alt="" className="w-32 cursor-pointer" />
+          <div className="flex w-1/3 items-center">
+            <img src={logoName} alt="" className="w-32" />
           </div>
         </div>
         <div className="w-full">
